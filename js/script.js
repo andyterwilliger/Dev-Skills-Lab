455a92ac0242b17bf60637e6ac32744ed8adc677
Andy Terwilliger <andy@terwilliger1@gmail.com>
2021-12-09 19:01:05 -0600

    Actually fixed js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-$('ul').on('click', 'li', function(){
+$('ul').on('click', 'button',  function(){
     $(this)
     .closest('li')
-    $(this).remove();
+    .remove();
 })
 
 
 $('#add-skill').on('click', function(evt){
     const newText = $(`input`).val()
-    $('ul').append($(`<li><button>X</button>${newText}</li>`))
+    $('ul').append($(`<li><button>X</button>  ${newText}</li>`))
     
-    console.log(evt)
+ 
 
 })
 
