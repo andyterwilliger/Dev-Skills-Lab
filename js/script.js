2a533d70f0092b2ac8c0c23fcee0c927a09d3b7e
Andy Terwilliger <andy@terwilliger1@gmail.com>
2021-12-09 18:06:57 -0600

    finished js

--- js/script.js
+++ js/script.js
@@ -6,5 +6,10 @@ $('ul').on('click', 'li', function(){
 
 
 $('#add-skill').on('click', function(evt){
-        
+    const newText = $(`input`).val()
+    $('ul').append($(`<li><button>X</button>${newText}</li>`))
+    
+    console.log(evt)
+
 })
+
